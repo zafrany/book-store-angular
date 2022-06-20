7ff74edb2f953135755d3d00fe0c3e8b8ff027b2
Yair Zafrany <karai.wot@gmail.com>
2022-06-20 11:29:31 +0300

    almost done

--- src/app/components/admin-edit-remove-book/admin-edit-remove-book.component.ts
+++ src/app/components/admin-edit-remove-book/admin-edit-remove-book.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Book } from 'src/app/models/book.model';
 import { User } from 'src/app/models/user.model';
@@ -18,8 +19,11 @@ export class AdminEditRemoveBookComponent implements OnInit {
   bookId : string|null = null;
   bookName: string|null = null;
   displayModal: boolean = false;
+  displayEditBookModal: boolean = false;
+  currentItem: Book|null = null;
+  editDetailsForm!: FormGroup;
 
-  constructor(private bookService: BookService,private usersService: UsersService, private router:Router) { }
+  constructor(private fb: FormBuilder, private bookService: BookService,private usersService: UsersService, private router:Router) { }
 
   ngOnInit(): void {
     this.bookService.booksData.subscribe((books)=>{
@@ -31,6 +35,32 @@ export class AdminEditRemoveBookComponent implements OnInit {
       this.users = users;
     })
     this.users = this.usersService.users;
+
+    this.editDetailsForm = this.fb.group({
+      bookId: ['', [Validators.required]],
+      bookName: ['', [Validators.required]],
+      bookPicLink: ['', [Validators.required]],
+    },
+    {
+      validators: [this.idIsUniqueVlidator.bind(this)]
+    }
+    );
+  }
+
+  idIsUniqueVlidator(control: AbstractControl) : ValidationErrors|null{
+    const newBookId: number = parseInt(control.get('bookId')?.value);
+    for(let book of this.books){
+      if(this.currentItem !== book && book.bookId === newBookId){
+        return {'idNotUnique' : true};
+      }
+    }
+    return null;
+  }
+
+  idNotUniqueMessage() {
+    if(this.editDetailsForm.errors?.['idNotUnique'])
+      return "new Id already exists for a diffent product."
+    return null;
   }
 
   updateBookName(bookName: string){
@@ -66,11 +96,47 @@ export class AdminEditRemoveBookComponent implements OnInit {
     }
   }
 
-  onYesClick() {
+  onRemoveClick(book: Book) {
+    this.currentItem = book;
+    this.displayModal = true;
+  }
 
+  onYesClick() {
+    this.displayModal = false;
+    this.bookService.removeBook(this.currentItem!);
+    for(let user of this.users){
+      for(let item of user.cart.items)
+      {
+        if(item.item.bookId === this.currentItem!.bookId)
+        this.usersService.changeItemQuantity(item, 0);
+      }
+    }
   }
 
   onNoClick(){
+    this.displayModal = false;
+  }
 
+  onUpdateBookClick(book: Book){
+    this.currentItem = book;
+    this.displayEditBookModal = true;
+    this.editDetailsForm.controls['bookId'].setValue(this.currentItem.bookId);
+    this.editDetailsForm.controls['bookName'].setValue(this.currentItem.bookName);
+    this.editDetailsForm.controls['bookPicLink'].setValue(this.currentItem.bookPictureLink);
+  }
+
+  onSubmitEditForm(){
+    this.currentItem!.bookId = parseInt(this.editDetailsForm.get('bookId')!.value);
+    this.currentItem!.bookName = this.editDetailsForm.get('bookName')!.value;
+    this.currentItem!.bookPictureLink = this.editDetailsForm.get('bookPicLink')!.value;
+    this.displayEditBookModal = false;
+  }
+
+  cancelUpdate(){
+    this.displayEditBookModal = false;
+  }
+
+  dashboardNavigate() {
+    this.router.navigate(['admin/managment']);
   }
 }
